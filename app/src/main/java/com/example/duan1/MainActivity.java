94febf3b2a6e5c6dcd33aca608477db8359d2d89
ThanhBinh4811 <binhntph50571@gmail.com>
2024-11-14 10:21:36 +0700

    Sửa man hinh

--- app/src/main/java/com/example/duan1/MainActivity.java
+++ app/src/main/java/com/example/duan1/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.duan1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.example.duan1.ManHinhLogin.ManHinh1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,16 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, ManHinh1.class);
-                startActivity(intent);
-                finish();
-
-            }
-        },3000);
-
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        // hekkk
+        //hellooooooooooooooooooooo
     }
 }
