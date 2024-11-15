bcc400212b0ddedf44ea5600e749287052e3c368
ThanhBinh4811 <binhntph50571@gmail.com>
2024-11-15 11:34:32 +0700

    Sửa man home va them su kien

--- app/src/main/java/com/example/duan1/Home/Home.java
+++ app/src/main/java/com/example/duan1/Home/Home.java
@@ -2,12 +2,12 @@ package com.example.duan1.Home;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -15,8 +15,11 @@ import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import com.example.duan1.ManHinhTT.TTCaNhanFragment;
 import com.example.duan1.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity {
@@ -24,7 +27,7 @@ public class Home extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private Toolbar toolBar;
     private NavigationView navigationView;
-
+    private BottomNavigationView bottomNavigationView;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +44,38 @@ public class Home extends AppCompatActivity {
         drawerLayout= findViewById(R.id.main);
         toolBar= findViewById(R.id.toolBar);
         navigationView= findViewById(R.id.navigationView);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+
+        setSupportActionBar(toolBar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolBar, 0, 0);
+        toggle.setDrawerIndicatorEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        toggle.syncState();
+
+        drawerLayout.addDrawerListener(toggle);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, TTCaNhanFragment.newInstance()).commit();
+        navigationView.setNavigationItemSelectedListener(item -> {
+            Fragment fragment = null;
+            int id= item.getItemId();
+            if(id == R.id.mDSDT){
+                toolBar.setTitle("Thong tin ca nhan");
+                fragment = TTCaNhanFragment.newInstance();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+
+            }
+            drawerLayout.closeDrawer(GravityCompat.START); // Đóng Drawer sau khi chọn
+            return true;
+
+        });
+
+
+
+
 
         //setup toolbar
         setSupportActionBar(toolBar);
@@ -50,6 +85,8 @@ public class Home extends AppCompatActivity {
 
     }
 
+
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home){
@@ -58,6 +95,8 @@ public class Home extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 
 
 }
---
+++ app/src/main/java/com/example/duan1/ManHinhTT/TTCaNhanFragment.java
@@ -0,0 +1,43 @@
+package com.example.duan1.ManHinhTT;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.duan1.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TTCaNhanFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TTCaNhanFragment extends Fragment {
+
+
+    public TTCaNhanFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static TTCaNhanFragment newInstance() {
+        TTCaNhanFragment fragment = new TTCaNhanFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_t_t_ca_nhan, container, false);
+    }
+}
