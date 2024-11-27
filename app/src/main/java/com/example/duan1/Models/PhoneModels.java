ffcf11e7e61cee81073dc7e296068e88285be730
ThanhBinh4811 <binhntph50571@gmail.com>
2024-11-27 21:23:09 +0700

    ds-> sp->gio hang

--- app/src/main/java/com/example/duan1/Models/PhoneModels.java
+++ app/src/main/java/com/example/duan1/Models/PhoneModels.java
@@ -5,27 +5,53 @@ public class PhoneModels {
     private String namePhone;
     private double rating;
     private String price;
+    private String moTa;
 
-    public PhoneModels(int image, String name, double rating, String price) {
+    public PhoneModels(int image, String namePhone, double rating, String price, String moTa) {
         this.image = image;
-        this.namePhone = name;
+        this.namePhone = namePhone;
         this.rating = rating;
         this.price = price;
+        this.moTa = moTa;
     }
 
     public int getImage() {
         return image;
     }
 
+    public void setImage(int image) {
+        this.image = image;
+    }
+
     public String getName() {
         return namePhone;
+    }
+
+    public void setNamePhone(String namePhone) {
+        this.namePhone = namePhone;
     }
 
     public double getRating() {
         return rating;
     }
 
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
     public String getPrice() {
         return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
     }
 }
